ea32fbc7b2678cf870ec4bddee312fee7c078ca3
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-11-13 11:47:26 +0100

    Initial Commit

---
+++ src/test/java/com/guillaume/squareGamesApi/SquareGamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guillaume.squareGamesApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareGamesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
