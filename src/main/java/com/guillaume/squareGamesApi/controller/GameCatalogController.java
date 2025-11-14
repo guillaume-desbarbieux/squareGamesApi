befc43088dc9a24f112c1f6cc401a285a54967bf
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-11-14 15:58:45 +0100

    Integrate additional endpoints in GameCatalogController to support game operations and enhance functionality.

--- src/main/java/com/guillaume/squareGamesApi/controller/GameCatalogController.java
+++ src/main/java/com/guillaume/squareGamesApi/controller/GameCatalogController.java
@@ -1,20 +1,89 @@
 package com.guillaume.squareGamesApi.controller;
 
 import com.guillaume.squareGamesApi.service.GameCatalog;
+import fr.le_campus_numerique.square_games.engine.CellPosition;
+import fr.le_campus_numerique.square_games.engine.Game;
+import fr.le_campus_numerique.square_games.engine.GameStatus;
+import fr.le_campus_numerique.square_games.engine.Token;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("games")
 public class GameCatalogController {
 
-    @Autowired
     GameCatalog gameCatalog;
+    public GameCatalogController(GameCatalog gameCatalog) {
+        this.gameCatalog = gameCatalog;
+    }
 
-    @GetMapping ("catalog")
-    public Collection<String> getGameIdentifiers(){
+    @GetMapping()
+    public Collection<String> getGameIdentifiers() {
         return gameCatalog.getGameIdentifiers();
     }
+/*
+    //Body : Json {identifier, playerCount, boardSize)
+    @PostMapping()
+    public ResponseEntity<UUID> createGame(@RequestBody Game game) {
+        return gameCatalog.create(game);
+    }
+
+    @GetMapping("/{gameId}/status")
+    public ResponseEntity<GameStatus> getGameStatus(@PathVariable UUID gameId) {
+        return gameCatalog.getStatus(gameId);
+    }
+
+    @GetMapping("/{gameId}/players")
+    public ResponseEntity<Set<UUID>> getPlayerIds(@PathVariable UUID gameId) {
+        return gameCatalog.getPlayers(gameId);
+    }
+
+    @GetMapping("/{gameId}/players/current")
+    public ResponseEntity<UUID> getCurrentPlayerId(@PathVariable UUID gameId) {
+        return gameCatalog.getCurrentPlayer(gameId);
+    }
+
+    @GetMapping("/{gameId}/board-size")
+    public ResponseEntity<Integer> getBoardSize(@PathVariable UUID gameId) {
+        return gameCatalog.getBoardSize(gameId);
+    }
+
+    @GetMapping("/{gameId}/tokens/board")
+    public ResponseEntity<Map<CellPosition, Token>> getBoardTokens(@PathVariable UUID gameId) {
+        return gameCatalog.getBoard(gameId);
+    }
+
+    @GetMapping("/{gameId}/tokens/remaining")
+    public ResponseEntity<Collection<Token>> getRemainingTokens(@PathVariable UUID gameId) {
+        return gameCatalog.getRemainingTokens(gameId);
+    }
+
+    @GetMapping("/{gameId}/tokens/removed")
+    public ResponseEntity<Collection<Token>> getRemovedTokens(@PathVariable UUID gameId) {
+        return gameCatalog.getRemovedTokens(gameId);
+    }
+
+    @GetMapping("/{gameId}/moves/remaining")
+    public ResponseEntity<Set<CellPosition>> getAllowedMovesFromRemaining(@PathVariable UUID gameId, @RequestParam String name) {
+        return gameCatalog.getAllowedMovesFromRemaining(gameId, name);
+    }
+
+    @GetMapping("/{gameId}/moves/board")
+    public ResponseEntity<Set<CellPosition>> getAllowedMovesFromBoard(@PathVariable UUID gameId, @RequestParam int x, @RequestParam int y) {
+        return gameCatalog.getAllowedMovesFromBoard(gameId, x, y);
+    }
+
+    //GameMove {source = board || remaining, name, fromX, fromY, @Required toX, @Required toY}
+    @PostMapping("/{gameId}/play")
+    public ResponseEntity<String> playMove(@PathVariable UUID gameId, @RequestBody GameMove gameMove) {
+        return gameCatalog.playMove(gameId, gameMove);
+    }
+
+ */
 }
