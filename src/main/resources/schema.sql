0672a3f5559e07781e3ffa50b97bdfa244b1a525
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-11-24 16:11:38 +0100

    Fix Foreign Keys on rules for Delete

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,32 +5,35 @@ CREATE TABLE IF NOT EXISTS game
     gameType  VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS player
+CREATE TABLE IF NOT EXISTS playerGame
 (
     uuid     VARCHAR(255) NOT NULL,
     gameUuid VARCHAR(255) NOT NULL,
     PRIMARY KEY (uuid, gameUuid),
-    FOREIGN KEY (gameUuid) REFERENCES game (uuid)
+    FOREIGN KEY (gameUuid) REFERENCES game (uuid) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS boardToken
 (
-    uuid       VARCHAR(255) PRIMARY KEY,
+    id         INT PRIMARY KEY AUTO_INCREMENT,
     gameUuid   VARCHAR(255) NOT NULL,
     playerUuid VARCHAR(255) NOT NULL,
+    name       VARCHAR(255) NOT NULL,
     x          INT          NOT NULL,
     y          INT          NOT NULL,
-    FOREIGN KEY (gameUuid) REFERENCES game (uuid),
-    FOREIGN KEY (playerUuid) REFERENCES player (uuid)
+    FOREIGN KEY (gameUuid) REFERENCES game (uuid) ON DELETE CASCADE ,
+    FOREIGN KEY (playerUuid, gameUuid) REFERENCES playerGame (uuid, gameUuid) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS removedToken
 (
-    uuid       VARCHAR(255) PRIMARY KEY,
+    id         INT PRIMARY KEY AUTO_INCREMENT,
     gameUuid   VARCHAR(255) NOT NULL,
     playerUuid VARCHAR(255) NOT NULL,
     name       VARCHAR(255) NOT NULL,
-    FOREIGN KEY (gameUuid) REFERENCES game (uuid),
-    FOREIGN KEY (playerUuid) REFERENCES player (uuid)
+    x          INT          NOT NULL,
+    y          INT          NOT NULL,
+    FOREIGN KEY (gameUuid) REFERENCES game (uuid) ON DELETE CASCADE ,
+    FOREIGN KEY (playerUuid, gameUuid) REFERENCES playerGame (uuid, gameUuid) ON DELETE CASCADE
 );
 
