781429c903b407111e68047a9d0fede21eec20d9
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-11-24 12:05:12 +0100

    Update database schema: restructure `game` table, add `player`, `boardToken`, and `removedToken` tables with relationships

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,36 @@
 CREATE TABLE IF NOT EXISTS game
 (
-    id            VARCHAR(200) PRIMARY KEY,
-    boardSize     INT          NOT NULL,
-    players       VARCHAR(200) NOT NULL,
-    boardTokens   VARCHAR(200) NOT NULL,
-    removedTokens VARCHAR(200) NOT NULL
+    uuid      VARCHAR(255) PRIMARY KEY,
+    boardSize INT          NOT NULL,
+    gameType  VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS player
+(
+    uuid     VARCHAR(255) NOT NULL,
+    gameUuid VARCHAR(255) NOT NULL,
+    PRIMARY KEY (uuid, gameUuid),
+    FOREIGN KEY (gameUuid) REFERENCES game (uuid)
+);
+
+CREATE TABLE IF NOT EXISTS boardToken
+(
+    uuid       VARCHAR(255) PRIMARY KEY,
+    gameUuid   VARCHAR(255) NOT NULL,
+    playerUuid VARCHAR(255) NOT NULL,
+    x          INT          NOT NULL,
+    y          INT          NOT NULL,
+    FOREIGN KEY (gameUuid) REFERENCES game (uuid),
+    FOREIGN KEY (playerUuid) REFERENCES player (uuid)
+);
+
+CREATE TABLE IF NOT EXISTS removedToken
+(
+    uuid       VARCHAR(255) PRIMARY KEY,
+    gameUuid   VARCHAR(255) NOT NULL,
+    playerUuid VARCHAR(255) NOT NULL,
+    name       VARCHAR(255) NOT NULL,
+    FOREIGN KEY (gameUuid) REFERENCES game (uuid),
+    FOREIGN KEY (playerUuid) REFERENCES player (uuid)
+);
+
