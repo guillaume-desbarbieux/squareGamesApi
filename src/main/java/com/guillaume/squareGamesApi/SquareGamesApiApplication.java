a1705be54c524ef669730ee8f1dd2062570826ef
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-11-27 09:12:03 +0100

    Adding Swagger Documentation

--- src/main/java/com/guillaume/squareGamesApi/SquareGamesApiApplication.java
+++ src/main/java/com/guillaume/squareGamesApi/SquareGamesApiApplication.java
@@ -1,9 +1,18 @@
 package com.guillaume.squareGamesApi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Square Games API",
+                version = "1.1",
+                description = "API documentation for managing Games in Square Games"
+        )
+)
 public class SquareGamesApiApplication {
 
 	public static void main(String[] args) {
